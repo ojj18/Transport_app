0165d7c9beb207e3df504d69c191c64e2d915cbc
ojj18 <manojojj511@gmail.com>
2022-07-18 23:54:05 +0530

    implementing the text dropdown

--- lib/Billing/print_status_screen.dart
+++ lib/Billing/print_status_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:transport_app/widgets/Ts_button.dart';
+import 'package:transport_app/widgets/Ts_text_dropdown.dart';
 
 class PrintStatusScreen extends StatefulWidget {
   const PrintStatusScreen({Key? key}) : super(key: key);
@@ -10,6 +12,9 @@ class PrintStatusScreen extends StatefulWidget {
 }
 
 class _PrintStatusScreenState extends State<PrintStatusScreen> {
+  final ScrollController _listScrollController = ScrollController();
+  final ScrollController _customScrollController = ScrollController();
+  String? groupValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +24,192 @@ class _PrintStatusScreenState extends State<PrintStatusScreen> {
         backgroundColor: const Color.fromRGBO(63, 81, 181, 1),
         title: const Text('Transport'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Printer Status",
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "Printer Not Connected!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Sender Details",
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 20,
+                ),
+              ),
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                        width: 1,
+                        color: const Color.fromRGBO(211, 211, 211, 1))),
+                child: TextFormField(
+                  style: const TextStyle(fontSize: 20),
+                  cursorHeight: 25,
+                  cursorColor: Colors.pinkAccent,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 5),
+                    hintText: "Phone Number",
+                    hintStyle: TextStyle(fontSize: 20, color: Colors.grey),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                        width: 1,
+                        color: const Color.fromRGBO(211, 211, 211, 1))),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Receiver Details",
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 20,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                        width: 1,
+                        color: const Color.fromRGBO(211, 211, 211, 1))),
+                child: const Text(
+                  "Phone Number",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                // width: 200,
+                child: Row(
+                  children: [
+                    radioList(
+                        radioValue: "TVM",
+                        radioGroupValue: groupValue,
+                        onChange: (value) {
+                          setState(() {
+                            groupValue = value;
+                          });
+                        }),
+                    radioList(
+                        radioValue: "GIN",
+                        radioGroupValue: groupValue,
+                        onChange: (value) {
+                          setState(() {
+                            groupValue = value;
+                          });
+                        }),
+                    radioList(
+                        radioValue: "VET",
+                        radioGroupValue: groupValue,
+                        onChange: (value) {
+                          setState(() {
+                            groupValue = value;
+                          });
+                        }),
+                    radioList(
+                        radioValue: "CHE",
+                        radioGroupValue: groupValue,
+                        onChange: (value) {
+                          setState(() {
+                            groupValue = value;
+                          });
+                        }),
+                    radioList(
+                        radioValue: "KAN",
+                        radioGroupValue: groupValue,
+                        onChange: (value) {
+                          setState(() {
+                            groupValue = value;
+                          });
+                        }),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                        width: 1,
+                        color: const Color.fromRGBO(211, 211, 211, 1))),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(10.0),
+        height: 65,
+        child: TsButton(
+          minWidth: double.infinity,
+          buttonText: "NEXT",
+          buttonColor: const Color.fromRGBO(63, 81, 181, 1),
+          onPressed: () {
+            //   Navigator.pushNamed(context, DashboardScreen.routeName);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget radioList(
+      {String? radioValue, String? radioGroupValue, dynamic onChange}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Radio<String>(
+            activeColor: Colors.pinkAccent,
+            value: radioValue!,
+            groupValue: radioGroupValue,
+            onChanged: onChange),
+        Text(radioValue),
+      ],
     );
   }
 }
--- lib/widgets/Ts_text_dropdown.dart
+++ lib/widgets/Ts_text_dropdown.dart
@@ -1,33 +1,63 @@
-// import 'package:flutter/material.dart';
+// ignore_for_file: must_be_immutable
 
-// class TsTextDropDown extends StatefulWidget {
-//   TsTextDropDown({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
 
-//   String? hintText;
+class TsDropDown extends StatefulWidget {
+  TsDropDown(
+      {Key? key,
+      this.hintText,
+      this.minHeight = 50,
+      this.maxHeight = 300,
+      this.minWidth = 100,
+      this.maxWidth = double.maxFinite,
+      this.initialValue = "",
+      this.items,
+      this.valueChanged})
+      : super(key: key);
+  String? hintText;
+  double minHeight;
+  double maxHeight;
+  double minWidth;
+  double maxWidth;
+  String? initialValue;
+  late List<DropdownMenuItem<dynamic>>? items = [];
+  ValueChanged? valueChanged;
 
-//   @override
-//   State<TsTextDropDown> createState() => _TsTextDropDownState();
-// }
+  @override
+  State<TsDropDown> createState() => _TsDropDownState();
+}
 
-// class _TsTextDropDownState extends State<TsTextDropDown> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: FormField(builder: (FormFieldState<String> state) {
-//         return InputDecorator(
-//           decoration: InputDecoration(
-//             hintText: widget.hintText,
-//             border: OutlineInputBorder(
-//               borderRadius: BorderRadius.circular(3),
-//               borderSide: const BorderSide(
-//                   width: 1, color: Color.fromRGBO(211, 211, 211, 1)),
-//             ),
-//           ),
-//           isEmpty: false,
-//           child: DropdownButtonHideUnderline(
-//               child: DropdownButton(items: [], onChanged: (value) {})),
-//         );
-//       }),
-//     );
-//   }
-// }
+class _TsDropDownState extends State<TsDropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: widget.minHeight,
+        maxHeight: widget.maxHeight,
+        minWidth: widget.minWidth,
+        maxWidth: widget.maxWidth,
+      ),
+      color: Colors.white,
+      child: FormField(builder: (FormFieldState<String> state) {
+        return InputDecorator(
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(3),
+              borderSide: const BorderSide(
+                  width: 1, color: Color.fromRGBO(211, 211, 211, 1)),
+            ),
+          ),
+          isEmpty: widget.initialValue == "",
+          child: DropdownButtonHideUnderline(
+              child: DropdownButton<dynamic>(
+                  value: widget.initialValue,
+                  items: widget.items,
+                  onChanged: (value) {
+                    widget.valueChanged!(value);
+                  })),
+        );
+      }),
+    );
+  }
+}
